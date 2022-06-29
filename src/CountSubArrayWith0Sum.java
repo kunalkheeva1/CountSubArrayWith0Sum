78d90f214d320154305ffe83e47617a6afaa630e
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-28 19:51:18 -0700

    method to count number of sub arrays in the array, whose sum are zeros, so do this, i will start putting sums in array and then if the sum is 0 then increase the count, or if there is same repeated sum then i will increase the count , if both the cases are false then i put sum in the map

--- src/CountSubArrayWith0Sum.java
+++ src/CountSubArrayWith0Sum.java
@@ -3,18 +3,23 @@ import java.util.HashMap;
 public class CountSubArrayWith0Sum {
 
     public static long countSubArraysWith0Sum(long arr[], int n ){
+        //get a map to store the sum
 
         HashMap<Long, Integer> map = new HashMap<>();
         long count = 0, sum= 0;
-
+        //run a loop
         for(int i = 0; i<n; i++){
             sum +=0;
+            //increment the count if sum is zero
             if(sum ==0){
                 count ++;
             }
+            //if map contains sum then increment the count as well
             if(map.containsKey(sum)){
                 count++;
             }
+            //else put the sum in the map with its required frequency
+            //also we used map.getordefault so that everytime the value stored with key is getting updated
             map.put(sum, map.getOrDefault(sum, 0)+1);
         }return count;
 
